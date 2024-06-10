a8c6e806a4aee1c06f85c58c7e08a9281011f439
JorisB <jorisbudin@gmail.com>
2024-06-10 10:19:32 +0200

    finish step3

--- server/app/routers/api/boats/router.js
+++ server/app/routers/api/boats/router.js
@@ -6,9 +6,15 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-const { browse } = require("../../../controllers/boatActions");
+const { browse, read, edit, add } = require("../../../controllers/boatActions");
 
 router.get("/", browse);
+
+router.get("/:id", read);
+
+router.put("/:id", edit);
+
+router.post("/", add);
 
 /* ************************************************************************* */
 
