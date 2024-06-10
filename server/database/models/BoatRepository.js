7722331928a674c4ff73888fc358addc6efa5cf8
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-06-10 12:25:22 +0200

    Step 5 done

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -7,14 +7,16 @@ class BoatRepository extends AbstractRepository {
 
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    const [rows] = await this.database.query(`select boat.name, boat.id , tile.coord_x , tile.coord_y , tile.has_treasure , tile.type  from ${this.table} INNER JOIN tile ON boat.coord_x = tile.coord_x && boat.coord_y = tile.coord_y`);
 
     // Return the array of boats
     return rows;
   }
 
-  async update(boat){
-    const [result] = await this.database.query(`Update boat set coord_x = ${boat.coord_x}, coord_y=${boat.coord_y} where id=${boat.id}`)
+  async update(boat) {
+    const [result] = await this.database.query(
+      `Update boat set coord_x = ${boat.coord_x}, coord_y=${boat.coord_y} where id=${boat.id}`
+    );
 
     return result.affectedRows;
   }
