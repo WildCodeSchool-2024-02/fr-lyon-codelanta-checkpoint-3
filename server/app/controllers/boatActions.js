73a0b96d5c0688c57da3b578182a44c84529e2eb
romain_delol <delol.romain@gmail.com>
2024-06-10 11:23:02 +0200

    step3 finished

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,28 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  // Extract the category data from the request body and params
+  const boat = {
+    ...req.body,
+    id: req.params.id,
+    coord_x: req.body.coord_x,
+    coord_y: req.body.coord_y,
+  };
+
+  try {
+    // Update the category in the database
+    await tables.boat.update(boat);
+
+    // Respond with HTTP 204 (No Content)
+    res.sendStatus(204);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
 module.exports = {
   browse,
+  edit,
 };
