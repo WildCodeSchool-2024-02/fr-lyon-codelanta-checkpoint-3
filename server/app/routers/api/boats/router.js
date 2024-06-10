cd703dcadd49118cc503071ec8ff75ea405858cc
Bastien Fournier <bastien.fournier06@gmail.com>
2024-06-10 12:36:33 +0200

    step 3 finish

--- server/app/routers/api/boats/router.js
+++ server/app/routers/api/boats/router.js
@@ -6,10 +6,11 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-const { browse } = require("../../../controllers/boatActions");
+const { browse, edit } = require("../../../controllers/boatActions");
 
 router.get("/", browse);
 
+router.put("/:id", edit);
 /* ************************************************************************* */
 
 module.exports = router;
