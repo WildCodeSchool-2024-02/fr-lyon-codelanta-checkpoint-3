fe73cde6dad4fe85fc116b5379e77e65d0124051
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-06-10 12:06:53 +0200

    step5

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -7,7 +7,13 @@ class BoatRepository extends AbstractRepository {
 
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+
+    const [rows] = await this.database.query(
+      `SELECT boat.*, tile.type, tile.has_treasure
+      FROM ${this.table} 
+      JOIN tile 
+      ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y`
+    );
 
     // Return the array of boats
     return rows;
