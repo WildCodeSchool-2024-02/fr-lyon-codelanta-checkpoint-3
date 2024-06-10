7522ca6b3ef477bbc7a494124760db8f1dac5627
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-06-10 12:07:57 +0200

    added middelware in boat files

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -2,7 +2,7 @@ const tables = require("../../database/tables");
 
 const browse = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
+    // Fetch all boats from the database asynchronously
     const boats = await tables.boat.readAll(tables.tile);
 
     // Respond with the boats in JSON format
@@ -13,21 +13,23 @@ const browse = async (req, res, next) => {
   }
 };
 
+
 const edit = async (req, res, next) => {
-  const boats = {
-    ...req.body,
-    id: req.params.id,
-  };
+
+  const boats = { ...req.body, id: req.params.id };
 
   try {
     await tables.boat.update(boats);
+
+    // Respond with HTTP 204 (No Content)
     res.sendStatus(204);
   } catch (err) {
+    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 module.exports = {
   browse,
-  edit,
+  edit
 };
--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -13,4 +13,4 @@ const tileExists = async (req, res, next) => {
   }
 };
 
-module.exports = tileExists;
+module.exports = {tileExists};
